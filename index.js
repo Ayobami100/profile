416b1e18f4c7685f9c507a0d22012f27a5eb9e8e
deewhy <deewhy2015@gmail.com>
2022-03-04 17:14:47 +0100

    profile page

---
+++ index.js
@@ -0,0 +1,19 @@
+
+let work = document.getElementById('work')
+
+work.innerHTML = "<p>I finsihed my NYSC 4 years ago which was held in katsina state "+
+"<br>and in 2019 i got my first non-tech job at a water factory for like a year<br>"+
+"and in 2020 I got another tech job where I worked as an IT Specialist in Datacrest Technologies for 5 months<br>"+
+"and in 2021 I did a 3 months intership at Sidmach Technologies<br>"+
+"and I am currently doing a 6 months intership in JM Tech ICT Centre</p>"
+
+work.style.fontSize = "20px"
+
+let about = document.getElementById('about')
+
+about.innerHTML = "<p>My name is Quadri Adedayo with the last name of Jamiu. <br>"+ 
+"I grew up in Osogbo Osun State, was born in the month of june in the year 1960, I am also from a family of 4<br>"+
+"I had my nursery and primary school in osogbo and also attended federal government college ikirun for my junior and secondary school level<br>"+
+"and I finished my higher institution in 2017...</p>"
+
+about.style.fontSize = "20px"
